76cfed44d4a638ea1b972ab2f808fdf36c589ca0
Hungpv <phamvanhung124@gmail.com>
2023-08-21 19:22:06 +0700

    create grid and add snake

---
+++ Snake/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            
+            for (int c = 0; c < 3; c++) {
+                Grid[r, c] = GridValue.Snake;
+                snakePosition.AddFirst(new Position(r, c));
+            }
+        }
+    }
+}
