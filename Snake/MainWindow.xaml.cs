7f37eff9e32f580ec8175c3c9c84c0a1f2300137
Hungpv <phamvanhung124@gmail.com>
2023-09-02 13:44:34 +0700

    draw dead snake completed

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Snake
             {Direction.Left, 270}
         };
 
-        private readonly int rows = 15, cols = 15;
+        private readonly int rows = 25, cols = 15;
         private readonly Image[,] gridImages;
         private GameState gameState;
         private bool gameRunning;
@@ -100,6 +100,7 @@ namespace Snake
             Image[,] images = new Image[rows, cols];
             GameGrid.Rows = rows;
             GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * (cols /(double)rows);
 
             for (int r = 0; r < rows; r++)
             {
@@ -149,9 +150,22 @@ namespace Snake
             image.RenderTransform = new RotateTransform(rotation);
         }
 
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.SnakePositions());
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+            }
+        }
+
         private async Task ShowCountDown()
         {
-            for(int i = 3; i >= 1; i--)
+            for (int i = 3; i >= 1; i--)
             {
                 OverlayText.Text = i.ToString();
                 await Task.Delay(500);
@@ -160,6 +174,7 @@ namespace Snake
 
         private async Task ShowGameOver()
         {
+            await DrawDeadSnake();
             await Task.Delay(1000);
             Overlay.Visibility = Visibility.Visible;
             OverlayText.Text = "PRESS ANY KEY TO START";
